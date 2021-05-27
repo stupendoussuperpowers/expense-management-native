3c86a163fff7d6f98814936854c3f6bc85fa7470
Fabhi <43674998+Fabhi@users.noreply.github.com>
2021-05-27 23:42:25 +0530

    Refactored: Moved the homepage into pages folder

---
+++ expense_management/lib/pages/Homepage.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:expense_management/pages/MainPage.dart';
+import './AddTransaction.dart';
+import './GroupsPage.dart';
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Expense Management'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedTab = 0;
+
+  // TODO: Replace with imports from these pages
+
+  @override
+  Widget build(BuildContext context) {
+    var pageLists = [
+      MainPage(), //Text('Home Page'),
+      AddTransaction(), //Text('Add Transaction Page'),
+      GroupsPage(), //Text('View Groups Page'),
+      Text('Settings Page'),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(child: pageLists[_selectedTab]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        onTap: (value) => setState(() {
+          _selectedTab = value;
+        }),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.black,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add,
+              color: Colors.black,
+            ),
+            label: 'Add Transaction',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.list,
+              color: Colors.black,
+            ),
+            label: 'View Groups',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.black,
+            ),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ expense_management/lib/themes/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+  static Color textColor = const Color(0xff9C9C9D);
+  static Color textColorDark = const Color(0xffffffff);
+
+  static Color bodyColor = const Color(0xFFFFFFFF);
+  static Color bodyColorDark = const Color(0xFF0E0E0F);
+
+  static Color buttonBackgroundColor = const Color(0xfff7f7f7);
+  static Color buttonBackgroundColorDark = const Color(0xff121212);
+}
